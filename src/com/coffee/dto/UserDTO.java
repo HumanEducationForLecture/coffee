78f94a649ec5b9bf939974e306f8795fdc7f8380
kwongiho <shng10503@naver.com>
2017-09-17 16:40:31 +0900

    user model

---
+++ src/com/coffee/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.coffee.dto;
+
+public class UserDTO implements User, Grade {
+	private String id;
+	private String password;
+	private int grade;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+
+}
